8259fc9e63da37e69e636c4eb8d138e17457efe9
yuhengtu <yuhengtuece@gmail.com>
2024-07-16 20:11:50 -0700

    visual

--- mirt.R
+++ mirt.R
@@ -1,7 +1,21 @@
-library(mirt)
+# install.packages("mirt")
+# install.packages("ggplot2")
+# install.packages("dplyr")
 
-data <- read.csv("clean_data/base_matrix.csv", row.names=1)
-print(data)
-model <- mirt(data, 1, '3PL')
-summary(model)
-plot(model)
+library(mirt)
+library(ggplot2)
+library(dplyr)
+
+data <- read.csv("./clean_data/perturb2_matrix.csv", row.names=1)
+
+items_to_remove <- which(apply(data, 2, function(x) length(unique(x)) == 1))
+cleaned_data <- data[, -items_to_remove]
+
+model <- mirt(cleaned_data, 1, '3PL')
+coef(model)
+coefficient <- coef(model)
+write.csv(coefficient, "./model_coef/perturb2_coef.csv")
+
+# summary(model)
+# plot(model)
+
