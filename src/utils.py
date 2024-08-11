800bc798e573254ce1d4a9ed5580271e0bc00e94
yuhengtu <yuhengtuece@gmail.com>
2024-08-10 22:24:54 -0700

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -23,28 +23,6 @@ def calculate_1d_wasserstein_distance(vector1, vector2):
 def calculate_3d_wasserstein_distance(matrix1, matrix2):
     return wasserstein_distance_nd(matrix1, matrix2)
 
-def plot_scatter(base_coef, perturb1_coef, perturb2_coef, axis_lim = False):
-    plt.rcParams.update({'font.size': 20})
-    fig = plt.figure(figsize=(7, 7))
-    ax = fig.add_subplot(111, projection='3d')
-
-    print(base_coef[:, 0])
-    ax.scatter(base_coef[:, 0], base_coef[:, 1], base_coef[:, 2], c='r', label='Base Coef')
-    ax.scatter(perturb1_coef[:, 0], perturb1_coef[:, 1], perturb1_coef[:, 2], c='g', label='Perturb1 Coef')
-    ax.scatter(perturb2_coef[:, 0], perturb2_coef[:, 1], perturb2_coef[:, 2], c='b', label='Perturb2 Coef')
-
-    ax.set_xlabel('a1')
-    ax.set_ylabel('d')
-    ax.set_zlabel('g')
-
-    if axis_lim:
-        ax.set_xlim(-3, 3)
-        ax.set_ylim(-3, 3)
-        ax.set_zlim(-3, 3)
-
-    ax.legend()
-    plt.show()
-
 if __name__ == "__main__":
     print(calculate_1d_wasserstein_distance([0, 1, 3], [5, 6, 8]))
     print(calculate_1d_wasserstein_distance([1, 1, 3], [5, 6, 8]))
