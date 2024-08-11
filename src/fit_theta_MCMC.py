1b290b0a2f3664730af1c7bf6e185690a1bd9ad0
yuhengtu <yuhengtuece@gmail.com>
2024-08-11 16:41:01 -0700

    MCMC

--- src/fit_theta_MCMC.py
+++ src/fit_theta_MCMC.py
@@ -8,13 +8,15 @@ from synthetic_testtaker import SimulatedTestTaker
 
 def model(Z, asked_question_list, asked_answer_list):
     theta_hat = numpyro.sample("theta_hat", dist.Normal(0.0, 1.0)) # prior
-    for asked_question_index in asked_question_list:
-        prob = item_response_fn_1PL(Z[asked_question_index], theta_hat, datatype="jnp")
-        numpyro.sample(
-            f"obs_{asked_question_index}", 
-            dist.Bernoulli(prob), 
-            obs=asked_answer_list[asked_question_index].astype(jnp.float32)
-            )
+    
+    Z_asked = Z[jnp.array(asked_question_list)]
+    probs = item_response_fn_1PL(Z_asked, theta_hat, datatype="jnp")
+    
+    numpyro.sample(
+        "obs", 
+        dist.Bernoulli(probs), 
+        obs=asked_answer_list.astype(jnp.float32)
+    )
 
 def fit_theta_with_mcmc(Z, asked_question_list, asked_answer_list, num_samples=2000, num_warmup=1000):
     rng_key = random.PRNGKey(0)
@@ -26,22 +28,16 @@ def fit_theta_with_mcmc(Z, asked_question_list, asked_answer_list, num_samples=2
         rng_key_, Z=Z, asked_question_list=asked_question_list, asked_answer_list=asked_answer_list
         )
     mcmc.print_summary()
-    samples_1 = mcmc.get_samples()
     
-    # jnp.expand_dims(?, -1): ?.shape = (n_samples,) -> (n_samples, 1)
-    posterior_mu = (
-        jnp.expand_dims(samples_1, -1)
-        + jnp.expand_dims(samples_1["bM"], -1) * dset.MarriageScaled.values
-    )
+    theta_samples = mcmc.get_samples()["theta_hat"]
+    mean_theta = jnp.mean(theta_samples)
+    variance_theta = jnp.var(theta_samples)
 
-    mean_mu = jnp.mean(posterior_mu, axis=0)
-    hpdi_mu = hpdi(posterior_mu, 0.9)
-        
     return mean_theta, variance_theta
 
 if __name__ == "__main__":
     random_key = random.PRNGKey(42)
-    question_num = 2
+    question_num = 1000
 
     z3 = random.normal(random_key, shape=(question_num,))
     new_testtaker = SimulatedTestTaker(z3, model="1PL", datatype="jnp")
