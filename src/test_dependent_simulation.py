d095a0066f5173be77dfc5aa02f02ee4499c8292
yuhengtu <yuhengtuece@gmail.com>
2024-08-14 15:03:24 -0700

    plot

--- src/test_dependent_simulation.py
+++ src/test_dependent_simulation.py
@@ -2,6 +2,7 @@ import torch
 from synthetic_testtaker import SimulatedTestTaker
 from fit_theta import fit_theta_mcmc
 import jax.numpy as jnp
+import matplotlib.pyplot as plt
 
 def inverse_item_response_fn_1PL(y,theta):
     return -theta - torch.log((1 - y) / y)
@@ -44,12 +45,34 @@ if __name__ == "__main__":
     
     asked_answer_list_1 = jnp.array(asked_answer_list_1)
     Z_1 = jnp.array(Z_1)
-    mean_theta_1, std_theta_1 = fit_theta_mcmc(Z_1, asked_question_list, asked_answer_list_1)
+    mean_theta_1, std_theta_1, theta_1_samples = fit_theta_mcmc(Z_1, asked_question_list, asked_answer_list_1)
     print(f"mean_theta_1: {mean_theta_1}")
     print(f"std_theta_1: {std_theta_1}")
     
     asked_answer_list_2 = jnp.array(asked_answer_list_2)
     Z_2 = jnp.array(Z_2)
-    mean_theta_2, std_theta_2 = fit_theta_mcmc(Z_2, asked_question_list, asked_answer_list_2)
+    mean_theta_2, std_theta_2, theta_2_samples= fit_theta_mcmc(Z_2, asked_question_list, asked_answer_list_2)
     print(f"mean_theta_2: {mean_theta_2}")
     print(f"std_theta_2: {std_theta_2}")
+    
+    
+    
+    plt.figure(figsize=(12, 6))
+
+    plt.subplot(1, 2, 1)
+    plt.hist(theta_1_samples, bins=30, density=True, alpha=0.6, color='blue', label='theta_1_samples')
+    plt.hist(theta_2_samples, bins=30, density=True, alpha=0.6, color='green', label='theta_2_samples')
+    plt.title('Histogram of Theta Samples')
+    plt.xlabel('Theta')
+    plt.ylabel('Density')
+    plt.legend()
+
+    plt.subplot(1, 2, 2)
+    plt.hist(Z_1, bins=30, density=True, alpha=0.6, color='red', label='Z_1')
+    plt.hist(Z_2, bins=30, density=True, alpha=0.6, color='orange', label='Z_2')
+    plt.title('Histogram of Z')
+    plt.xlabel('Z')
+    plt.ylabel('Density')
+    plt.legend()
+
+    plt.savefig('../plot/synthetic/test_dependent_simulation.png')
