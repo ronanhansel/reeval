43 appendix 3 variance problem fix (#47)

src/CAT_MCMC.py
@@ -8,9 +8,6 @@ import numpy as np
 from argparse import ArgumentParser
 import os
 
-# export JAX_PLATFORM_NAME=cpu
-# nohup python CAT.py > /scratch/yuhengtu/workspace/certified-eval/src/CAT.log 2>&1 &
-  
 def CAT_owen(z3, unasked_question_list, theta_mean):
     z3_unasked = z3[unasked_question_list]
     z3_unasked = jnp.array(z3_unasked)
@@ -126,8 +123,6 @@ def main(question_num, new_testtaker, strategy, subset_question_num, warmup, sta
         clear_caches()
         
 if __name__ == "__main__":
-    # debug python CAT.py --subset_question_num 5
-    
     parser = ArgumentParser()
     parser.add_argument("--seed", type=int, default=10)
     parser.add_argument("--algo", type=str, default="fisher")
@@ -149,4 +144,4 @@ if __name__ == "__main__":
         subset_question_num=args.subset_question_num, 
         warmup=args.warmup,
         state_dir = state_dir
-        )
+    )

src/fit_theta.py
@@ -4,45 +4,41 @@ from numpyro.infer import MCMC, NUTS
 import jax.numpy as jnp
 import jax.random as random
 from utils import item_response_fn_1PL
-from numpyro.diagnostics import hpdi
 import torch
 import torch.optim as optim
-from utils import item_response_fn_3PL, item_response_fn_2PL, item_response_fn_1PL
+from utils import item_response_fn_1PL
 from testtaker import SimulatedTestTaker
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 def fit_theta_mle(Z, asked_question_list, asked_answer_list, epoch=300):
-    theta_hat = torch.normal(mean=0.0, std=1.0, size=(1,), requires_grad=True)
+    Z = Z.cuda()
+    asked_question_list = asked_question_list.cuda()
+    asked_answer_list = asked_answer_list.cuda()
+    
+    theta_hat = torch.normal(mean=0.0, std=1.0, size=(1,), requires_grad=True, device='cuda')
     optimizer = optim.Adam([theta_hat], lr=0.01)
+    
     for _ in range(epoch):
         log_prob = 0
-        for asked_question_index in asked_question_list:
-            # prob = item_response_fn_3PL(*Z[asked_question_index, :], theta_hat)
+        for i, asked_question_index in enumerate(asked_question_list):
             prob = item_response_fn_1PL(Z[asked_question_index], theta_hat)
             bernoulli = torch.distributions.Bernoulli(prob)
-            log_prob = log_prob + bernoulli.log_prob(asked_answer_list[asked_question_index].float())
+            log_prob = log_prob + bernoulli.log_prob(asked_answer_list[i].float())
         
-        loss = -log_prob/len(asked_question_list)
+        loss = -log_prob / len(asked_question_list)
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
         # print(theta_hat)
-        
-    return theta_hat
+
+    return theta_hat.cpu()
 
 def model(Z, asked_question_list, asked_answer_list):
     theta_hat = numpyro.sample("theta_hat", dist.Normal(0.0, 1.0)) # prior
     Z_asked = Z[asked_question_list]
     probs = item_response_fn_1PL(Z_asked, theta_hat, datatype="jnp")
     numpyro.sample("obs", dist.Bernoulli(probs), obs=asked_answer_list)
-
-# def model(asked_question_list, asked_answer_list):
-#     Z_asked = numpyro.sample("Z", dist.Normal(0.0, 1.0, (asked_question_list.size(),)))
-#     theta_hat = numpyro.sample("theta_hat", dist.Normal(0.0, 1.0)) # prior
-#     Z_asked = Z[asked_question_list]
-#     probs = item_response_fn_1PL(Z_asked, theta_hat, datatype="jnp")
-#     numpyro.sample("obs", dist.Bernoulli(probs), obs=asked_answer_list)
 
 def fit_theta_mcmc(Z, asked_question_list, asked_answer_list, num_samples=9000, num_warmup=1000):
     rng_key = random.PRNGKey(0)
@@ -121,8 +117,10 @@ if __name__ == "__main__":
         asked_answer_list.append(new_testtaker.ask(z3, i))
     
     # MLE
-    # theta_hat = fit_theta_mle(z3, asked_question_list, asked_answer_list, epoch=300)
-    # print(f"mle theta: {theta_hat}")
+    asked_question_tensor = torch.tensor(asked_question_list)
+    asked_answer_tensor = torch.tensor(asked_answer_list)
+    theta_hat = fit_theta_mle(z3, asked_question_tensor, asked_answer_tensor, epoch=300)
+    print(f"mle theta: {theta_hat}")
 
     # MCMC
     asked_question_list = jnp.array(asked_question_list)