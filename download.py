926b062e94f3322bcbe91ce4268fb4a7b580fc6b
Sang T. Truong <sttruong@cs.stanford.edu>
2025-06-17 15:30:17 +0700

    Update download.py

--- download.py
+++ download.py
@@ -1,21 +1,11 @@
-import argparse
 from huggingface_hub import snapshot_download
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--folder",
-    type=str,
-    default="data", # result
-)
-args = parser.parse_args()
-
-folder = args.folder
-repo_id = f"stair-lab/reeval_{folder}_public"
+repo_id = f"stair-lab/reeval"
 
 snapshot_download(
     repo_id=repo_id,
     repo_type="dataset",
-    local_dir=folder,
+    local_dir="data",
 )
 
 print(f"Downloaded `{repo_id}` into `./{folder}`")
